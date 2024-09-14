7604fae5c59c3d74abc2f14ac00522243965a778
mahmadasghar <146083682+mahmadasghar@users.noreply.github.com>
2024-09-14 16:53:35 +0500

    upload assignment-18-form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Components/Form/Form'
+import Heading from './Components/Heading/Heading'
+
+const App = () => {
+  return (
+    <div>
+      <Heading/>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Heading/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Heading.css'
+const Heading = () => {
+  return (
+    <div>
+      <h1>Registration Form</h1>
+    </div>
+  )
+}
+
+export default Heading
